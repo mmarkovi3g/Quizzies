b693745670517d480d0ae9f7aa8c899f753505ff
Mario Marković <mmarkovi3g@gmail.com>
2023-08-01 09:49:59 +0200

    Small changes
    - Tried ChatGpt to generate list of questions into Array
    - implemented buttons for navigation and logic
    *** Need to learn more how to effectively render one question at the time according to question counter
    *** I will update CSS as soon as possible

--- src/App.js
+++ src/App.js
@@ -2,11 +2,99 @@ import "./App.css";
 import Question from "./components/Question";
 import "./App.css";
 
+const questions = [
+  {
+    category: "geography",
+    q: "What is the capital of Croatia?",
+    a1: "Belgrade",
+    a2: "Ljubljana",
+    a3: "Sarajevo",
+    qa: "Zagreb",
+  },
+  {
+    category: "geography",
+    q: "Which sea lies to the west of Croatia?",
+    a1: "Mediterranean Sea",
+    a2: "Black Sea",
+    a3: "Caspian Sea",
+    qa: "Adriatic Sea",
+  },
+  {
+    category: "history",
+    q: "Who was the first King of Croatia?",
+    a1: "Gjuro",
+    a2: "Ivan",
+    a3: "Nikola",
+    qa: "Tomislav",
+  },
+  {
+    category: "culture",
+    q: "Which traditional Croatian instrument is often associated with the Dalmatian region?",
+    a1: "Accordion",
+    a2: "Bagpipes",
+    a3: "Guitar",
+    qa: "Tamburica",
+  },
+  {
+    category: "food",
+    q: "What is the traditional Croatian dish often made with lamb, potatoes, and vegetables?",
+    a1: "Pizza",
+    a2: "Sarma",
+    a3: "Rozata",
+    qa: "Peka",
+  },
+  {
+    category: "landmarks",
+    q: "Which UNESCO World Heritage Site in Croatia is known for its well-preserved medieval architecture?",
+    a1: "Osijek",
+    a2: "Dubrovnik",
+    a3: "Šibenik",
+    qa: "Trogir",
+  },
+  {
+    category: "sports",
+    q: "Which sport is very popular in Croatia and often played on the beach?",
+    a1: "Volleyball",
+    a2: "Handball",
+    a3: "Football",
+    qa: "Water Polo",
+  },
+  {
+    category: "language",
+    q: "Which language family does the Croatian language belong to?",
+    a1: "Romance",
+    a2: "American",
+    a3: "Germanic",
+    qa: "Slavic",
+  },
+  {
+    category: "festivals",
+    q: "Which Croatian city hosts the annual music festival called 'INmusic'?",
+    a1: "Zadar",
+    a2: "Split",
+    a3: "Osijek",
+    qa: "Zagreb",
+  },
+];
+
 function App() {
   return (
     <div>
       <div>Wellcome to Quizzies</div>
-      <Question />
+      <div>
+        {questions.map((question) => (
+          <Question
+            question={questions}
+            key={question.q}
+            q={question.q}
+            a1={question.a1}
+            a2={question.a2}
+            a3={question.a3}
+            qa={question.qa}
+            category={question.category}
+          />
+        ))}
+      </div>
       <div>FOOTER</div>
     </div>
   );
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,21 +1,44 @@
 import "./Question.css";
+import { useState } from "react";
 
-export default function Question() {
+export default function Question(question) {
+  const numOfQ = 10;
+  const [qNum, setQNum] = useState(1);
+
+  const nextHandler = () => {
+    if (qNum < numOfQ) {
+      setQNum((prev) => prev + 1);
+    }
+  };
+
+  const prevHandler = () => {
+    if (qNum > 1) {
+      setQNum((prev) => prev - 1);
+    }
+  };
+
   return (
     <div className="question">
+      <p className="question_number">
+        <span>{qNum}</span>/{numOfQ}
+      </p>
       <div className="header">
         <div className="image">
           <img src="/media/img/question_img/zagreb.jpg" alt="answer" />
         </div>
-        <div className="text">Kako se zove glavni grad Republike Hrvatske?</div>
+        <div className="text">{question.q}</div>
       </div>
       <div className="answers">
         <ul>
-          <li>Beograd</li>
-          <li>Ljubljana</li>
-          <li>Zagreb</li>
-          <li>Sarajevo</li>
+          <li>{question.a1}</li>
+          <li>{question.a2}</li>
+          <li>{question.a3}</li>
+          <li>{question.qa}</li>
         </ul>
+      </div>
+      <div className="navigation">
+        <button onClick={prevHandler}>Previous</button>
+        <button onClick={nextHandler}>Next</button>
       </div>
     </div>
   );
