78ef3d573b0595ded3ae9c9d723580502c34facb
Mario Marković <mmarkovi3g@gmail.com>
2023-08-03 16:09:18 +0200

    Some small changes
    * organized some css
    * added functionality to show / hide input form (not sure if i will need that, just to try out)

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,32 +1,46 @@
-import "./Question.css";
+import "./Form.css";
+import { useState } from "react";
 
 export default function Form() {
+  const [isOpen, setIsOpen] = useState(false);
+  function toggleHandler() {
+    isOpen === false ? setIsOpen(true) : setIsOpen(false);
+  }
+
   return (
     <div className="form">
-      <form>
-        <p>Choose you Quizzies settings to generate your own Quiz: </p>
+      <div className="form_header">
+        <p>Choose you Quizzies settings to generate your own Quizzie: </p>
+        <p className="toggler" onClick={toggleHandler}>
+          {isOpen === false ? "+" : "x"}
+        </p>
+      </div>
+      {isOpen === true ? (
+        <form>
+          <div className="form_wrap">
+            <div>
+              <p>Select category: </p>
+              <select>
+                <option>Geography</option>
+                <option>Sports</option>
+                <option>History</option>
+              </select>
+            </div>
 
-        <div className="form_wrap">
-          <div>
-            <p>Select category: </p>
-            <select>
-              <option>Geography</option>
-              <option>Sports</option>
-              <option>History</option>
-            </select>
+            <div>
+              <p>Select number of questions: </p>
+              <select>
+                <option>10</option>
+                <option>20</option>
+                <option>30</option>
+              </select>
+            </div>
           </div>
-
-          <div>
-            <p>Select number of questions: </p>
-            <select>
-              <option>10</option>
-              <option>20</option>
-              <option>30</option>
-            </select>
-          </div>
-        </div>
-        <button>Generate Quizzie</button>
-      </form>
+          <button>Generate Quizzie</button>
+        </form>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
