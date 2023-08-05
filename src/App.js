6ccd5dd47d9f628000962cd7d698af5f67fa08a3
Mario Marković <mmarkovi3g@gmail.com>
2023-08-05 16:07:19 +0200

    Added random questions functionality
    * now every time i refresh i get 10 random questions
    * also did some state changes in component
    * finally working as it should
    
    * now need to implement no of questios in form and category

--- src/App.js
+++ src/App.js
@@ -6,31 +6,16 @@ import questions from "./questions.json";
 import { useState } from "react";
 
 function App() {
-  const [currentQ, setCurrentQ] = useState(0);
-  const question = questions[currentQ];
-
-  const numOfQuestions = questions.length;
+  //needs to be declared here because if i declared it inside Question jsx it would render with every state change
+  const randomPitanja = Array.from({ length: 10 }, () =>
+    Math.floor(Math.random() * questions.length)
+  );
 
   return (
     <div>
       <div className="title">Wellcome to Quizzies</div>
-
       <Form />
-
-      {questions.map((question) => (
-        <Question
-          question={question}
-          qNumber={questions.indexOf(question) + 1}
-          numOfQuestions={questions.length}
-          key={question.q}
-          q={question.question}
-          a1={question.optionA}
-          a2={question.optionB}
-          a3={question.optionC}
-          a4={question.optionD}
-          category={question.category}
-        />
-      ))}
+      <Question questions={questions} randomPitanja={randomPitanja} />
 
       <div></div>
     </div>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,26 +1,54 @@
+import { useState } from "react";
 import "./Question.css";
 
-export default function Question(question) {
+export default function Question({ questions, randomPitanja }) {
+  //generating random array of indexes
+
+  //counting questions for later use
+  const numOfQuestions = randomPitanja.length;
+
+  //for looping around random array
+  const [index, setIndex] = useState(0);
+  const [currentQ, setCurrentQ] = useState(randomPitanja[index]);
+
+  //getting the displayed question
+  const question = questions[currentQ];
+
+  //next question handler
+  function nextHandler(event) {
+    event.preventDefault();
+    if (index < numOfQuestions - 1) {
+      setIndex((prev) => prev + 1);
+      setCurrentQ(randomPitanja[index + 1]);
+    } else {
+      // Reset the index to 0 to loop back to the first question
+      setIndex(9);
+      setCurrentQ(randomPitanja[index]);
+    }
+  }
+
   return (
     <div className="question">
       <p className="question_number">
-        <span>{question.qNumber}</span>/{question.numOfQuestions}
+        <span>{index + 1}</span>/{randomPitanja.length}
       </p>
       <div className="header">
-        <div className="text">{question.q}</div>
+        <div className="text">{question.question}</div>
       </div>
       <div className="answers">
         <ul>
-          <li onClick={question.onNextHandler}>{question.a1}</li>
-          <li onClick={question.onNextHandler}>{question.a2}</li>
-          <li onClick={question.onNextHandler}>{question.a3}</li>
-          <li onClick={question.onNextHandler}>{question.a4}</li>
+          <li onClick={nextHandler}>{question.optionA}</li>
+          <li onClick={nextHandler}>{question.optionB}</li>
+          <li onClick={nextHandler}>{question.optionC}</li>
+          <li onClick={nextHandler}>{question.optionD}</li>
         </ul>
       </div>
+      {/*
       <div className="navigation">
-        <button onClick={question.onPreviousHandler}>Previous</button>
-        <button onClick={question.onNextHandler}>Next</button>
-      </div>
+        <button>Previous</button>
+        <button>Next</button>
+      </div>{" "}
+      */}
     </div>
   );
 }
