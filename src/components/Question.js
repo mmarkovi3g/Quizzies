d82f24afe01d1e2ff3787ff75213667f202808d1
Mario Marković <mmarkovi3g@gmail.com>
2023-08-09 18:39:18 +0200

    Added logic to count score
    * changed <li> elements to buttons
    * added score window which shows final score
    
    I dont know if this is the best way of doing quizz but i will keep it like this for now.

--- src/components/Question.js
+++ src/components/Question.js
@@ -11,6 +11,12 @@ export default function Question({ questions, randomPitanja }) {
   //looping through questions using random index
   const [currentQ, setCurrentQ] = useState(randomPitanja[index]);
 
+  //setting up state to show results window
+  const [finished, setFinished] = useState(false);
+
+  //setting up score
+  const [score, setScore] = useState(0);
+
   //next question handler
   function nextHandler(event) {
     event.preventDefault();
@@ -21,7 +27,14 @@ export default function Question({ questions, randomPitanja }) {
       // Reset the index to 0 to loop back to the first question
       setIndex(9);
       setCurrentQ(randomPitanja[index]);
+      setFinished(true);
     }
+    const answer = event.target.value;
+    if (answer === question.answer) {
+      setScore((prevScore) => prevScore + 1);
+    }
+    console.log(event.target.value);
+    console.log(question.answer);
   }
 
   //getting the displayed question
@@ -31,35 +44,54 @@ export default function Question({ questions, randomPitanja }) {
   const category = question.category;
 
   return (
-    <div className="question">
-      <div className="header">
-        <img
-          className="category-img"
-          src={`./media/img/${category}.png`}
-          alt="category"
-        />
-        <p className="question_number">
-          <span>{index + 1}</span>/{randomPitanja.length}
-        </p>
-      </div>
+    <div>
+      {finished ? (
+        <div className="question-completed">
+          <h2>Quizzie completed</h2>
+          <p>
+            You answered correctly {score} of {numOfQuestions} questions
+          </p>
+        </div>
+      ) : (
+        <div className="question">
+          <div className="header">
+            <img
+              className="category-img"
+              src={`./media/img/${category}.png`}
+              alt="category"
+            />
+            <p className="question_number">
+              <span>{index + 1}</span>/{randomPitanja.length}
+            </p>
+          </div>
 
-      <div className="header">
-        <div className="text">{question.question}</div>
-      </div>
-      <div className="answers">
-        <ul>
-          <li onClick={nextHandler}>{question.optionA}</li>
-          <li onClick={nextHandler}>{question.optionB}</li>
-          <li onClick={nextHandler}>{question.optionC}</li>
-          <li onClick={nextHandler}>{question.optionD}</li>
-        </ul>
-      </div>
-      {/*
-      <div className="navigation">
-        <button>Previous</button>
-        <button>Next</button>
-      </div>{" "}
-      */}
+          <div className="header">
+            <div className="text">{question.question}</div>
+          </div>
+          <div className="answers">
+            <ul>
+              <button onClick={nextHandler} value={question.optionA}>
+                {question.optionA}
+              </button>
+              <button onClick={nextHandler} value={question.optionB}>
+                {question.optionB}
+              </button>
+              <button onClick={nextHandler} value={question.optionC}>
+                {question.optionC}
+              </button>
+              <button onClick={nextHandler} value={question.optionD}>
+                {question.optionD}
+              </button>
+            </ul>
+          </div>
+          {/*
+  <div className="navigation">
+    <button>Previous</button>
+    <button>Next</button>
+  </div>{" "}
+  */}
+        </div>
+      )}
     </div>
   );
 }
